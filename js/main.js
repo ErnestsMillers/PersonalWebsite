32d9da43b8dca2b65f8706c6469a775bc76e5c37
Ernests Millers <atrcepolnes@inbox.lv>
2016-09-19 23:06:30 +0100

    Finished main page

--- js/main.js
+++ js/main.js
@@ -57,3 +57,29 @@ $('a[href^="#"]').on('click', function(event) {
         }, 1000);
     }
 });
+
+
+$("a[href='#top']").click(function() {
+  $("html, body").animate({ scrollTop: 0 }, "slow");
+  return false;
+});
+
+
+$(document).ready(function(){
+
+	//Check to see if the window is top if not then display button
+	$(window).scroll(function(){
+		if ($(this).scrollTop() > 500) {
+			$('.scrollToTop').fadeIn();
+		} else {
+			$('.scrollToTop').fadeOut();
+		}
+	});
+
+	//Click event to scroll to top
+	$('.scrollToTop').click(function(){
+		$('html, body').animate({scrollTop : 0},800);
+		return false;
+	});
+
+});
